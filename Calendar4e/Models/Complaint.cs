688b439e4da05fd63b4d79718351ad7c0adbe4d6
Anastasia <434559@student.fontys.nl>
2019-12-13 15:47:15 +0100

    commit

---
+++ Calendar4e/Models/Complaint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Calendar4e.Models
+{
+    public class Complaint
+    {
+        [Key]
+        public Int64 id { get; set; }
+
+        public String title { get; set; }
+
+        public String description { get; set; }
+        public String date { get; set; }
+        public String email { get; set; }
+    }
+}
